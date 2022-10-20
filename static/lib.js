9e074f856f702a007174a850c489998f86fd36d6
Jagger <634750802@qq.com>
2022-10-20 18:30:01 +0800

    fix

--- static/lib.js
+++ static/lib.js
@@ -1,4 +1,4 @@
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch("/main.wasm", { headers: { 'accept': 'application/wasm' }}), go.importObject).then((result) => {
+WebAssembly.instantiateStreaming(fetch(document.baseURI + "main.wasm", { headers: { 'accept': 'application/wasm' }}), go.importObject).then((result) => {
     go.run(result.instance);
 });
